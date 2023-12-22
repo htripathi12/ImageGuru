929924c24ced7bcd21459ac5614665738a06b09f
htripathi12 <tripathh@purdue.edu>
2023-12-21 23:52:16 -0500

    final

--- hello.py
+++ hello.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
+from keras import regularizers, optimizers
 
 # Load CIFAR-10 dataset
 (train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
@@ -21,22 +22,19 @@ model = models.Sequential()
 
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
 model.add(layers.MaxPooling2D((2, 2)))
-
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
-
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-
+model.add(layers.Dropout(0.2))
 model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(64, activation='relu', kernel_regularizer=regularizers.l2(0.001)))
 model.add(layers.Dense(10, activation='softmax'))
 
-# Compile the model
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-# Train the model
+# # Train the model
 history = model.fit(X_train, y_train, epochs=15, validation_data=(X_valid, y_valid))
 
 # Plot training history
@@ -48,11 +46,11 @@ plt.ylim([0, 1])
 plt.legend(loc='lower right')
 plt.show()
 
-# Evaluate the model on the test set
+# # Evaluate the model on the test set
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 print(f"\nTest accuracy: {test_acc}")
 
-# Make predictions and generate a classification report and confusion matrix
+# # Make predictions and generate a classification report and confusion matrix
 predictions = model.predict(test_images)
 predictions_classes = [tf.argmax(prediction).numpy() for prediction in predictions]
 
@@ -61,7 +59,7 @@ class_names = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "
 print("\nClassification Report:")
 print(classification_report(test_labels, predictions_classes, target_names=class_names))
 
-# Plot confusion matrix
+# # Plot confusion matrix
 cm = confusion_matrix(test_labels, predictions_classes)
 plt.figure(figsize=(8, 8))
 sns.heatmap(cm, annot=True, fmt='d', xticklabels=class_names, yticklabels=class_names)
